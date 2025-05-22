e3521082015543ed07bb3f2668876bdfc3486cdf
satchelspencer <satchelspencer@gmail.com>
2025-05-21 18:38:58 -0600

    fix fixed output name

--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 for converting to and from git-friendly [hsrs](https://github.com/satchelspencer/hsrs) deck format
 
 - `hsrs-import ./target_dir source.json` convert json to file structure in target_dir
-- `hsrs-export ./output_dir ./source_dir` convert file structure in source_dir to json
+- `hsrs-export output.json ./source_dir` convert file structure in source_dir to json
--- src/export.js
+++ src/export.js
@@ -4,9 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const { getOutput } = require('./output')
 
-const distDir = process.argv[2]
-if (!fs.existsSync(distDir)) fs.mkdirSync(distDir, { recursive: true })
-fs.writeFileSync(
-  path.join(distDir, 'jp.deck.json'),
-  JSON.stringify(getOutput(process.argv[3]))
-)
+const outputPath = process.argv[2],
+  dirPath = path.dirname(outputPath)
+if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
+fs.writeFileSync(outputPath, JSON.stringify(getOutput(process.argv[3])))
